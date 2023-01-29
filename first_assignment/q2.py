ada723cbb8c91c00da0c51f8abe83d60f069739e
Mostafa <s.mostafa.a96@gmail.com>
2023-01-29 00:57:22 -0500

    second question

---
+++ first_assignment/q2.py
@@ -0,0 +1,68 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def dirac(inp):
+    return np.where(inp == 0, np.inf, 0)
+
+
+def discrete_dirac(inp):
+    return np.where(inp == 0, 1, 0)
+
+
+def rect(x):
+    return np.where(abs(x) < 0.5, 1, 0) + np.where(abs(x) == 0.5, 0.5, 0)
+
+
+def item_a():
+    print(dirac(np.array(list(range(-2, 3)))))
+
+
+def item_b():
+    n = np.arange(-10, 11, 1)
+    d = discrete_dirac(n)
+    plt.xticks(n)
+    plt.stem(n, d)
+    plt.xlabel('n')
+    plt.ylabel('dirac(n)')
+    plt.show()
+
+
+def item_c():
+    x = np.arange(-5, 5.1, 0.1)
+    y = rect(x)
+    plt.plot(x, y)
+    plt.xlabel('x')
+    plt.ylabel('rect(x)')
+    plt.show()
+
+
+def item_d():
+    x = np.arange(-5 * np.pi, 5 * np.pi, 0.1)
+    y = np.sinc(np.pi * x / 2) * 2 / np.pi
+    plt.plot(x, y)
+    plt.xlabel('x')
+    plt.ylabel('function(x)')
+    plt.show()
+
+
+def item_e():
+    u0 = 1
+    u1 = 1
+    x, y = np.meshgrid(np.arange(-3, 3, 0.05), np.arange(-3, 3, 0.05))
+    z = np.sin(u0 * x + u1 * y)
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(x, y, z)
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    plt.show()
+
+
+if __name__ == "__main__":
+    # item_a()
+    # item_b()
+    # item_c()
+    # item_d()
+    item_e()
