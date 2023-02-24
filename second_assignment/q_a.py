c03a057de62ced2bff2aaccf88ac3fbee28b589f
Mostafa <s.mostafa.a96@gmail.com>
2023-02-23 19:14:35 -0500

    fist item done

--- second_assignment/q_a.py
+++ second_assignment/q_a.py
@@ -1,14 +1,16 @@
-import numpy as np
 import scipy.io as sio
-import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     rf_file = sio.loadmat('./data/mousekidney.mat')['RawRF']
 
-    fs = 500 * 10**6  # Sampling frequency in Hz
+    fs = 500 * 10 ** 6  # Sampling frequency in Hz
     c = 1540  # Speed of sound in m/s
-    f0 = 7e6  # Center frequency in Hz
     N = rf_file.shape[0]  # Number of samples
     L = rf_file.shape[1]  # Number of transducer elements
-    depth = 40e-3  # Depth of image in meters
-    print(rf_file.shape)
+    lateral_dimension = 0.02  # Lateral dimension of the image in meters
+
+    # Each line has 10000 samples and the sampling frequency is 500 * 10^6. So, sampling time is:
+    sampling_time = N / fs
+    # And we have to see in that time, how much the sound signal can travel?
+    max_depth = sampling_time * c
+    print(f'{max_depth} meters')
