53d4eb0cc4266e1b6cba4269e82a5cd85021676c
Mostafa <s.mostafa.a96@gmail.com>
2023-02-27 08:32:28 -0500

    minor change

--- second_assignment/q_c.py
+++ second_assignment/q_c.py
@@ -1,3 +1,10 @@
 if __name__ == "__main__":
-    print("""Cause it shows the most frequent item. in different depths, we can see different frequencies. 
-    for example in some parts it is not changing, in other parts it is changing mostly with some frequency""")
+    print("""If you look at the previous diagrams, all of them have a rise in (0, 0). 
+    And going deeper, the point goes higher. Eventually, in the third diagram, 
+    it is higher than the peak around 23 MHz! 
+    It is a pretty interesting experiment since it proves the wave is losing energy 
+    and more specifically, its details. 
+    Traveling through tissue, sound waves encounter variations in the density, speed of sound, 
+    and attenuation of the tissue. As the sound waves penetrate deeper into the tissue, 
+    they encounter higher-density and higher-attenuation tissue that slows them 
+    and causes them to lose their energy. """)
